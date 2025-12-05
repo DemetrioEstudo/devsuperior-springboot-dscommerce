e92ff75280398defb5408d82762cdbe5f8f7cbbb
FlavioDemetrio <flademetrio@gmail.com>
2025-12-05 14:37:54 -0300

    iniciado projeto feito Order, OrderStatus, Payment, User e configurado banco de dados h2

---
+++ src/main/java/br/com/klsys/dscommerce/entities/Order.java
@@ -0,0 +1,74 @@
+package br.com.klsys.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    @OneToOne(mappedBy ="order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order() {
+    }
+
+    public Order(long id, Instant moment, OrderStatus orderStatus, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        this.orderStatus = orderStatus;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+}
---
+++ src/main/java/br/com/klsys/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package br.com.klsys.dscommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+}
