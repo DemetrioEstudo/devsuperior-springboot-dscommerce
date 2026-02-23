5f585802b29263fc0a8bcb76e833ddf705a11ccd
Flavio Antonio Demetrio (linux) <flaviodemetrio@gmail.com>
2026-02-23 19:53:12 -0300

    get usuario logado

--- src/main/java/br/com/klsys/dscommerce/dto/UserDTO.java
+++ src/main/java/br/com/klsys/dscommerce/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package br.com.klsys.dscommerce.dto;
 
 import br.com.klsys.dscommerce.entities.User;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -15,13 +16,64 @@ public class UserDTO {
 
     private List<String> roles = new ArrayList<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
     public UserDTO(User entity) {
          this.id = entity.getId();
          this.name = entity.getName();
          this.email = entity.getEmail();
          this.phone = entity.getPhone();
          this.birthDate = entity.getBirthDate();
-         entity.getRoles().forEach(role -> this.roles.add(role.getAuthority()));
+         for (GrantedAuthority role : entity.getRoles()) {
+             this.roles.add(role.getAuthority());
+         }
+
     }
 
 }
